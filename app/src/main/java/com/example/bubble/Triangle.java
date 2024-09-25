e8a23479669534e6deede66c24cc7c836206d0ba
smuir1 <semuir@ualberta.ca>
2024-09-24 19:22:55 -0600

    Added white color

--- app/src/main/java/com/example/bubble/Triangle.java
+++ app/src/main/java/com/example/bubble/Triangle.java
@@ -5,12 +5,14 @@ public class Triangle extends Shape{
     // ATTRIBUTES
     private int height;
     private int base;
+    String color = "white";
 
     // CONSTRUCTOR
     public Triangle(int x, int y, int base, int height, String color){
         super(x,y,color);
         this.height = height;
         this.base = base;
+        this.color=color
     }
 
     // GETTERS AND SETTERS
@@ -19,6 +21,7 @@ public class Triangle extends Shape{
     public void setColor(String color){this.color=color;}
     public int getHeight(){return height;}
     public int getBase(){return base;}
+    public String getColor(String color){return color;}
 
     // METHODS
 }
