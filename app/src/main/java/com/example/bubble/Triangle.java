5d396b9f3890a873d0bc07066fbfaa604eb1a716
smuir1 <semuir@ualberta.ca>
2024-09-24 19:36:51 -0600

    Fixed merge error

--- app/src/main/java/com/example/bubble/Triangle.java
+++ app/src/main/java/com/example/bubble/Triangle.java
@@ -5,23 +5,23 @@ public class Triangle extends Shape{
     // ATTRIBUTES
     private int height;
     private int base;
-    String color = "white";
+    String triangleColor = "white";
 
     // CONSTRUCTOR
     public Triangle(int x, int y, int base, int height, String color){
         super(x,y,color);
         this.height = height;
         this.base = base;
-        this.color=color
+        this.triangleColor=color
     }
 
     // GETTERS AND SETTERS
     public void setHeight(int height){this.height=height;}
     public void setBase(int base){this.base=base;}
-    public void setColor(String color){this.color=color;}
+    public void setTriangleColor(String color){this.triangleColor=color;}
     public int getHeight(){return height;}
     public int getBase(){return base;}
-    public String getColor(String color){return color;}
+    public String getTriangleColor(){return triangleColor;}
 
     // METHODS
 }
