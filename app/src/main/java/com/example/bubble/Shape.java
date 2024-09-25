a5245cb834c84c3362dfa54caaa585ef2dd9ada3
ChesterLuna <chester99124@gmail.com>
2024-09-24 18:58:39 -0600

    Create Shape.java

---
+++ app/src/main/java/com/example/bubble/Shape.java
@@ -0,0 +1,24 @@
+package com.example.bubble;
+
+public class Shape {
+
+    //ATTRIBUTES
+    private int x=0;
+    private int y=0;
+
+    //CONSTRUCTOR
+    public Shape(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+
+
+    //GETTERS AND SETTERS
+    public int getX(){return x;}
+    public int getY(){return y;}
+    public void setX(int x){this.x=x;}
+    public void setY(int y){this.y=y;}
+
+    // METHODS
+
+}
